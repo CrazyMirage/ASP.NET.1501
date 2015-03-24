d3e84302e80229c8b7351f411b7d523f866fda36
CrazyMirage <Anton.Yakouchyk@gmail.com>
2015-03-24 22:53:27 +0300

    Day8 (Part 1) missing part

---
+++ ASP.NET.1501.Yakouchyk.Day8/Extension/Extension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension
+{
+    public static class Extension
+    {
+        public static IEnumerable<T> GetEnumerable<T>(this IEnumerable<T> collection, Func<T, bool> solver)
+        {
+            foreach (T element in collection)
+            {
+                if (solver(element))
+                    yield return element;
+            }
+        }
+    }
+}
